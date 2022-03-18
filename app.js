edd940d33bd2f70055793d89f7f74b8d88be76de
Rayan Hadjari <rayan.hadjari@learnet.se>
2022-03-18 14:56:44 +0100

    appjs done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let watchedStore = JSON.parse(localStorage.getItem("watchedEpisodes")) || [];
-
 let episodes = Array.from(document.querySelectorAll("[type='checkbox']"));
+
+watchedStore.forEach((episode) => {
+  episodes[episode - 1].checked = true;
+});
 
 episodes.forEach((episode) => {
   episode.addEventListener("change", updateCheckBoxes);
@@ -12,7 +15,7 @@ function updateCheckBoxes() {
   );
   let watchedEpisodes = [];
   watched.forEach((watched) => {
-      watchedEpisodes.push(watched.getAttribute("data-episode"));
+    watchedEpisodes.push(watched.getAttribute("data-episode"));
   });
   localStorage.setItem("watchedEpisodes", JSON.stringify(watchedEpisodes));
 }
